dea2e8bd8f2f07f3b1dc8c134aa90199f6c81721
Natalman <natalnahm@gmail.com>
2020-03-05 19:17:42 -0600

    Added an image and changed the icon of the app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Beat Coin'),
         backgroundColor: Colors.brown[500],
       ),
-    ),));
+      body: Center(
+        child: Image(
+          image: AssetImage('images/bitcoin.jpg'),
+        ),
+      ),
+    ),
+  ));
 }
